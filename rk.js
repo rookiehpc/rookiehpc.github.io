bad75e5ee46805fd8ba33b6dcdcf54a9bfb74eee
RookieHPC <contact@rookiehpc.com>
2022-05-21 01:21:31 +0100

    Article links width adapting to search bar width.

--- rk.js
+++ rk.js
@@ -1728,6 +1728,16 @@ const RK = {
         // Hide the loader
         const Loader = document.getElementById('Loader');
         // Loader.parentElement.removeChild(Loader);
+        RK.ResizeSearchResultList();
+    },
+
+    ResizeSearchResultList: () => {
+        const SearchBar = document.getElementById('SearchBar');
+        const CompStyles = window.getComputedStyle(SearchBar);
+        const SearchBarWidth = parseInt(CompStyles.getPropertyValue('width').split("px")[0]);
+
+        let ArticleLinks = document.getElementById('ArticleLinks');
+        ArticleLinks.style.width = SearchBarWidth + "px";
     },
 
     EnableDarkMode: () => {
